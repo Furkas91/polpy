49f24dcc9cbf57d6c6b27a7b9a1cb90064757304
Vladimir Toropov <toropov29@rambler.ru>
2020-11-15 15:47:40 +0300

    added initial unit test.

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 from polynom import Polynom
 from polynom import Poly
 
+
 class TestPolynom(unittest.TestCase):
 
     def setUp(self):
@@ -11,6 +12,14 @@ class TestPolynom(unittest.TestCase):
     def test_add(self):
         result = self.polynom.__add__(self.otherpolynom).__str__()
         self.assertEqual(result, "x^3+2x^2+6x")
+
+    def test_sub(self):
+        result = self.polynom.__sub__(self.otherpolynom).__str__()
+        self.assertEqual(result, "x^3+3x+3")
+
+    def test_mul(self):
+        result = self.polynom.__mul__(self.otherpolynom).__str__()
+        self.assertEqual(result, "x^5+6x^4+x^3+5x^2+6x+3")
 
 
 if __name__ == "__main__":
