609c60a5952f6199d2c843eaf9167fe8aacfc41e
Vladimir Toropov <toropov29@rambler.ru>
2020-11-15 16:35:48 +0300

    added test_mul, test_floordiv, test_mod

--- test.py
+++ test.py
@@ -21,6 +21,14 @@ class TestPolynom(unittest.TestCase):
         result = self.polynom.__mul__(self.otherpolynom).__str__()
         self.assertEqual(result, "x^5+6x^4+x^3+5x^2+6x+3")
 
+    def test_floordiv(self):
+        result = self.polynom.__floordiv__(self.otherpolynom).__str__()
+        self.assertEqual(result, "x+3")
+
+    def test_mod(self):
+        result = self.polynom.__mod__(self.otherpolynom).__str__()
+        self.assertEqual(result, "5x+6")
+
 
 if __name__ == "__main__":
     unittest.main()
